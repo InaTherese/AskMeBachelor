9b6e4502cdb671fe8740ad54a60f15f8d8f9cf93
Håvard <Hvard@158.38.186.159>
2012-02-27 12:44:12 +0100

    mjau

---
+++ src/main/java/com/hist/bacheloraskme/model/question/RadioQuestion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hist.bacheloraskme.model.question;
+
+/**
+ *
+ * @author Håvard
+ */
+public class RadioQuestion extends Question{
+    public RadioQuestion(String question, int amountOfAnswers, String[] answers) {
+        super(question, amountOfAnswers, answers);
+    }
+}
---
+++ src/main/java/com/hist/bacheloraskme/model/question/YesNoQuestion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hist.bacheloraskme.model.question;
+
+/**
+ *
+ * @author Håvard
+ */
+
+public class YesNoQuestion extends Question {
+ 
+    public YesNoQuestion(String question, int amountOfAnswers, String[] answers) {
+        super(question, amountOfAnswers, answers);
+        amountOfAnswers = 2;
+        answers = new String[]{"yes", "no"};
+    }
+}
